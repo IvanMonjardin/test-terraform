526a1ee1a7a6871d6288d0416a1a7e79d1c1b7cd
IvanMonjardin <71083961+IvanMonjardin@users.noreply.github.com>
2022-02-21 12:06:39 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 ##############################################################################
 provider "ibm" {
   /* Uncomment ibmcloud_api_key while testing from CLI */
-  ibmcloud_api_key = var.api_key
+  /* ibmcloud_api_key = var.api_key */
   generation       = 2
   region           = "us-south"
   ibmcloud_timeout = 300
